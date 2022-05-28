a2896a51ed03f28f43192c2166ecbea0670c5b49
Joel Angel <joelbaraka4325@gmail.com>
2022-05-28 18:11:21 +0300

    recleaned the files

---
+++ assets/colors/colors.js
@@ -0,0 +1,36 @@
+// const colors = {
+//   background: '#F9F9FB',
+//   textDark: '#313234',
+//   textDark: '#F5CA48',
+//   secondary: '#F26C68',
+//   textLight: '#CDCDCD',
+//   price: '#E4723C',
+// };
+
+// export default colors;
+
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  White: {
+    background: '#F9F9FB',
+  },
+  textDark: {
+    color: '#313234',
+  },
+
+  Primary: {
+    color: '#F5CA48',
+  },
+  Secondary: {
+    color: '#F26C68',
+  },
+  TextLight: {
+    color: '#CDCDCD',
+  },
+  Price: {
+    color: '#E4723C',
+  },
+});
+
+export default styles;
