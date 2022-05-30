f9490a4f1fca8c2292ab9818e3aef644d61ea33f
Joel Angel <joelbaraka4325@gmail.com>
2022-05-30 16:22:24 +0300

    added the styling to the infO WRAPPER

--- components/Details.js
+++ components/Details.js
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   StyleSheet,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 import React from 'react';
 
@@ -41,10 +42,34 @@ const Details = ({route, navigation}) => {
 
       {/* Price */}
       <View style={styles.priceWrapper}>
-        <Text style={styles.priceText}>{item.price}</Text>
+        <Text style={styles.priceText}>${item.price}</Text>
       </View>
 
       {/* Pizza Info */}
+
+      <View style={styles.infoWrapper}>
+        <View style={styles.infoLeftWrapper}>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Size</Text>
+            <Text style={styles.infoItemText}>
+              {item.sizeName} {item.sizeNumber}"
+            </Text>
+          </View>
+
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Crust</Text>
+            <Text style={styles.infoItemText}>{item.crust}</Text>
+          </View>
+
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Delivery</Text>
+            <Text style={styles.infoItemText}>{item.deliveryTime} mins</Text>
+          </View>
+        </View>
+        <View style={styles.infoRightWrapper}>
+          <Image source={item.image} style={styles.itemImage} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -100,5 +125,41 @@ const styles = StyleSheet.create({
     color: colors.price,
     fontFamily: 'Poppins-Bold',
     fontSize: 32,
+  },
+
+  infoWrapper: {
+    marginTop: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  infoLeftWrapper: {
+    paddingLeft: 20,
+  },
+
+  infoItemWrapper: {
+    marginBottom: 20,
+  },
+
+  infoItemTitle: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 14,
+    color: colors.textLight,
+  },
+
+  infoItemText: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 18,
+    color: colors.textDark,
+  },
+
+  itemImage: {
+    resizeMode: 'contain',
+    marginLeft: 50,
+  },
+
+  ingredientsWrapper: {
+    marginTop: 40,
   },
 });
