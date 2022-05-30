e2ead053aaf092ee95ac861b6155ab696df91048
Joel Angel <joelbaraka4325@gmail.com>
2022-05-30 11:24:18 +0300

    installed react navigation

---
+++ assets/data/categoriesData.js
@@ -0,0 +1,29 @@
+const categoriesData = [
+  {
+    id: 1,
+    image: require('../images/pizza-icon.png'),
+    title: 'Pizza',
+    selected: true,
+  },
+
+  {
+    id: 1,
+    image: require('../images/shrimp-icon.png'),
+    title: 'Seafood',
+    selected: false,
+  },
+
+  {
+    id: 1,
+    image: require('../images/soda-icon.png'),
+    title: 'Soft Drinks',
+    selected: false,
+  },
+
+  {
+    id: 1,
+    image: require('../images/pizza-icon.png'),
+  },
+];
+
+export default categoriesData;
---
+++ assets/data/popularData.js
@@ -0,0 +1,27 @@
+const popularData = [
+  {
+    id: 1,
+    image: require('../images/pizza1.png'),
+    title: 'Primavera Pizza',
+    weight: '540 gr',
+    rating: '5.0',
+  },
+
+  {
+    id: 1,
+    image: require('../images/shrimp-icon.png'),
+    title: 'Vegetarian Pizza',
+    weight: '540 gr',
+    rating: '4.0',
+  },
+
+  {
+    id: 1,
+    image: require('../images/soda-icon.png'),
+    title: 'Pepperoni Pizza',
+    weight: '700 gr',
+    rating: '5.0',
+  },
+];
+
+export default popularData;
